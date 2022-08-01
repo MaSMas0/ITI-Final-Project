39520df675af8ceead53e398210f6516df97bd39
Ahmed Ragheb <a7medra8eb97@gmail.com>
2022-08-01 10:13:40 +0200

    productCard-nav

--- ECommerceApp/navigation/AppNavigator.js
+++ ECommerceApp/navigation/AppNavigator.js
@@ -1,28 +1,51 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
 import Home from '../screens/Home';
 import Category from '../screens/Category';
 const Tab = createBottomTabNavigator();
 
-const AppNavigator = () => (
-  <Tab.Navigator>
+const Footer = () => (
+  <Tab.Navigator 
+  screenOptions={{
+    tabBarActiveTintColor:'black',
+    // headerShown:false
+  }}>
+   
     <Tab.Screen
       name="Home"
       component={Home}
       options={{
-        tabBarIcon: ({color, size}) => (
+        tabBarIcon: ({ focused,color, size}) => (
           <MaterialCommunityIcons name="home" color={color} size={size} />
         ),
       }}
     />
+    
     <Tab.Screen
       name="Category"
       component={Category}
       options={{
         tabBarIcon: ({color, size}) => (
-          <MaterialCommunityIcons
-            name="drag-indicator"
+          <MaterialIcons
+            name="category"
+            color={color}
+            size={size}
+          />
+        ),
+      }}
+    />
+
+    <Tab.Screen
+      name="Settings"
+      component={Category}
+      options={{
+        tabBarIcon: ({color, size}) => (
+          <AntDesign
+            name="setting"
             color={color}
             size={size}
           />
@@ -32,4 +55,4 @@ const AppNavigator = () => (
   </Tab.Navigator>
 );
 
-export default AppNavigator;
+export default Footer;
--- ECommerceApp/screens/Home.js
+++ ECommerceApp/screens/Home.js
@@ -2,29 +2,56 @@ import React from 'react';
 import {FlatList, ScrollView, StyleSheet, Text, View} from 'react-native';
 import SlideShow from '../components/SlideShow';
 import BrandCard from '../components/BrandCard';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import colors from '../config/colors';
+
 const Home = props => {
   return (
+
+      
     <View style={styles.container}>
+      
       <SlideShow />
-      <Text>Brands</Text>
+      <Text style={styles.line}></Text>
+      <Text style={styles.h1}>Brands</Text>
+      <Text style={styles.line}></Text>
+
+  
 
       <FlatList
         nestedScrollEnabled
-        numColumns={2}
+         numColumns={2}
         data={[1, 2, 3, 45, 5, 6, 7, 8, 8, 8, 8, 8, 8, 8]}
         renderItem={(item, index) => {
           return <BrandCard />;
         }}
       />
     </View>
+
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor:Colors.white,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  line:{
+    width:"100%",
+    height:8,
+    backgroundColor:colors.lightGrey,
+    // backgroundColor:"#F6F6F6",
+    margin:5
+  },
+  h1:{
+    fontSize:24,
+    textTransform:'uppercase',
+    fontWeight:"bold",
+    color:colors.black,
+
+  }
+
 });
 
 export default Home;
