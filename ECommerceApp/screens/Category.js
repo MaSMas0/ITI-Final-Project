0fcb32f85cee0eb89c9d3ddd68ac34c195b9411a
kareem Ansary <kareem.ansary@gmail.com>
2022-08-01 02:45:51 +0200

    Create-Screen-Home

---
+++ ECommerceApp/screens/Category.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+function Category(props) {
+  return (
+    <View style={styles.container}>
+      <View></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default Category;
