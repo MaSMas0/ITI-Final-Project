3efeb0aee2df2ac97e41d57a54734247fb29310d
kareem Ansary <kareem.ansary@gmail.com>
2022-08-01 03:33:19 +0200

    Connect CategoryScreen with AppNav

--- ECommerceApp/navigation/AppNavigator.js
+++ ECommerceApp/navigation/AppNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from '../screens/Home';
+import Category from '../screens/Category';
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
@@ -12,6 +13,19 @@ const AppNavigator = () => (
       options={{
         tabBarIcon: ({color, size}) => (
           <MaterialCommunityIcons name="home" color={color} size={size} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Category"
+      component={Category}
+      options={{
+        tabBarIcon: ({color, size}) => (
+          <MaterialCommunityIcons
+            name="drag-indicator"
+            color={color}
+            size={size}
+          />
         ),
       }}
     />
