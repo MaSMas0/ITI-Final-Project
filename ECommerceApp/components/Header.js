345d6f7c69ef7efe3e61068005a91c398bbd0c0d
kareem Ansary <kareem.ansary@gmail.com>
2022-08-01 19:26:04 +0200

    Implment-SearchBarLogic with Animation-Home

--- ECommerceApp/components/Header.js
+++ ECommerceApp/components/Header.js
@@ -7,15 +7,26 @@ function Header(props) {
   return (
     <View style={styles.headerContainer}>
       <SearchBar />
-      <AntDesign name="shoppingcart" size={25}></AntDesign>
-      <AntDesign name="heart" size={25}></AntDesign>
+      <View
+        style={{
+          flexDirection: 'row',
+          flex: 1,
+          justifyContent: 'space-around',
+        }}>
+        <AntDesign name="shoppingcart" size={25}></AntDesign>
+        <AntDesign name="heart" size={25}></AntDesign>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   headerContainer: {
+    flex: 1,
+    paddingEnd: 20,
     flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 });
 export default Header;
--- ECommerceApp/components/SearchBar.js
+++ ECommerceApp/components/SearchBar.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, TextInput, View} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import colors from '../config/colors';
 import {Searchbar} from 'react-native-paper';
+import * as Animatable from 'react-native-animatable';
 const SearchBar = () => {
   const [searchQuery, setSearchQuery] = React.useState('');
+  const [icon, setIcon] = useState(false);
   const onChangeSearch = query => setSearchQuery(query);
   return (
-    <Searchbar
-      placeholder="Search"
-      onChangeText={onChangeSearch}
-      value={searchQuery}
-    />
+    <View style={styles.serachContainer}>
+      {!icon && (
+        <Animatable.View
+          animation="zoomIn"
+          duration={500}
+          style={styles.serachContainer}>
+          <AntDesign name="search1" size={22} onPress={() => setIcon(!icon)} />
+        </Animatable.View>
+      )}
+      {icon && (
+        <Animatable.View
+          animation="zoomIn"
+          duration={500}
+          style={styles.serachContainer}>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+            style={{backgroundColor: 'white', elevation: 1}}
+            onIconPress={() => setIcon(!icon)}
+          />
+        </Animatable.View>
+      )}
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  serachContainer: {
+    flex: 3,
+  },
+});
 export default SearchBar;
