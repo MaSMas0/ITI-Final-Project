57d3aef0fdf8f187a573e9f94fc660da59798cd4
mahmoudsalem1907 <62968091+mahmoudsalem1907@users.noreply.github.com>
2022-08-03 01:41:04 +0200

    About and contact Us and setting profile screen

--- ECommerceApp/screens/Settings.js
+++ ECommerceApp/screens/Settings.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import AboutScreen from './AboutScreen';
+import ContactUs from './ContactUs';
+import SettingProfile from './SettingProfile';
+// import AboutScreen from './AboutScreen';
 
 function Settings(props) {
   return (
     <View style={styles.container}>
-      <View>
-        <Text>Settings</Text>
-      </View>
+      {/* <AboutScreen />
+      <ContactUs /> */}
+      <SettingProfile />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    color: 'red',
+    fontSize: 56,
+  },
 });
 
 export default Settings;
