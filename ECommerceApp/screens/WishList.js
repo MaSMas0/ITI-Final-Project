16a2b7ce3ca01f86cd3a90fb4d4c1e89632a101b
Amr El-Sokkary <105977445+Sokkar95@users.noreply.github.com>
2022-08-03 05:05:09 +0200

    ui_ of_ wishlist_ page

--- ECommerceApp/screens/WishList.js
+++ ECommerceApp/screens/WishList.js
@@ -1,42 +1,18 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, View, Text} from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet} from 'react-native';
 import WishlistComponent from '../components/WishlistComponant';
-import AntDesign from 'react-native-vector-icons/AntDesign';
+
 import colors from '../config/colors';
 
 const WishList = () => {
   return (
-    <SafeAreaView
-      style={{
-        marginTop: 20,
-        paddingHorizontal: 10,
-      }}>
-      <View
-        style={{
-          marginVertical: 10,
-          marginBottom: 30,
-          //   paddingBottom: 10,
-          //   borderBottomWidth: 1,
-          borderColor: 'grey',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}>
-        <AntDesign name="left" size={25} />
-        <View
-          style={{
-            justifyContent: 'flex-start',
-          }}>
-          <Text
-            style={{
-              color: colors.black,
-              fontSize: 25,
-              fontWeight: 'bold',
-            }}>
-            WishList
-          </Text>
-        </View>
-      </View>
+    <SafeAreaView>
       <ScrollView>
+        <WishlistComponent />
+        <WishlistComponent />
+        <WishlistComponent />
+        <WishlistComponent />
+        <WishlistComponent />
         <WishlistComponent />
       </ScrollView>
     </SafeAreaView>
