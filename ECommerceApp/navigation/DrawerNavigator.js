646eb35e99fba861116895c2cca1d4f1754bb7fb
kareem Ansary <kareem.ansary@gmail.com>
2022-08-03 02:22:51 +0200

    config-DrawerNav-Style

--- ECommerceApp/navigation/DrawerNavigator.js
+++ ECommerceApp/navigation/DrawerNavigator.js
@@ -1,12 +1,32 @@
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {DrawerActions} from '@react-navigation/native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import React from 'react';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import Settings from '../screens/Profile';
 
 const Drawer = createDrawerNavigator();
 
-const DrawerNavigator = () => {
+const DrawerNavigator = ({navigation}) => {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      screenOptions={{
+        drawerPosition: 'right',
+        drawerType: 'slide',
+        drawerPosition: 'right',
+        headerLeft: false,
+        headerRight: () => (
+          <TouchableOpacity
+            onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
+            <AntDesign
+              name="setting"
+              size={27}
+              color="black"
+              style={{marginRight: 5}}
+            />
+          </TouchableOpacity>
+        ),
+      }}>
       <Drawer.Screen name="profile" component={Settings} />
     </Drawer.Navigator>
   );
