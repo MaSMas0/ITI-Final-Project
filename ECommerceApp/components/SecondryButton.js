d002c5ac1c1dd20eedd751a2220e9053d990a3f9
karimansary97 <kareem.ansary@gmail.com>
2022-08-04 03:22:14 +0200

    Make-secondaryButton-Customized

--- ECommerceApp/components/SecondryButton.js
+++ ECommerceApp/components/SecondryButton.js
@@ -4,14 +4,15 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import LinearGradient from 'react-native-linear-gradient';
 import colors from '../config/colors';
 
-function SecondryButton({onPress, title}) {
+function SecondryButton({onPress, title, style}) {
+  console.log({...style}, 'esta');
   return (
     <TouchableOpacity onPress={onPress}>
       <LinearGradient
         start={{x: 1, y: 0}}
         end={{x: 0, y: 0}}
         colors={['#030A4E', '#22336a']}
-        style={styles.submit}>
+        style={[styles.submit, style]}>
         <Text style={styles.submit}>
           <FontAwesome name="cart-plus" size={20}></FontAwesome> {title}
         </Text>
