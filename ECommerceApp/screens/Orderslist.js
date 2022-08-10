603a58424239af3d946f5139fc6d78c224511360
karimansary97 <kareem.ansary@gmail.com>
2022-08-10 08:36:56 +0200

    put not found in orders screen

--- ECommerceApp/screens/Orderslist.js
+++ ECommerceApp/screens/Orderslist.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import {TouchableOpacity, ScrollView, View, FlatList} from 'react-native';
+import {TouchableOpacity, Text, ScrollView, View, FlatList} from 'react-native';
 import OrderDetails from './OrderDetails';
 const Orders = ({navigation}) => {
+  const x = [];
   return (
     <View>
-      <FlatList
-        data={[0, 0, 0, 0, 0, 0, 0, 0]}
-        renderItem={() => {
-          return (
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate('MoreOrderItemDetails');
-              }}>
-              <OrderDetails />
-            </TouchableOpacity>
-          );
-        }}
-      />
+      {x.length == 0 && (
+        <View style={{alignItems: 'center', justifyContent: 'center'}}>
+          <Text>notfound</Text>
+        </View>
+      )}
+
+      {x.length !== 0 && (
+        <FlatList
+          data={x}
+          renderItem={() => {
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('MoreOrderItemDetails');
+                }}>
+                <OrderDetails />
+              </TouchableOpacity>
+            );
+          }}
+        />
+      )}
     </View>
   );
 };
