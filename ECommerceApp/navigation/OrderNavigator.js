87506028a0d72757cd3c3f6bd47f1d4d54af479e
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-08-13 00:31:22 +0200

    add Main order details in order navigation

--- ECommerceApp/navigation/OrderNavigator.js
+++ ECommerceApp/navigation/OrderNavigator.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import OrderCard from '../components/OrderCard';
 import Orders from '../screens/Orderslist';
+import MainOrderDetails from '../screens/MainOrderDetails';
 
 const Stack = createNativeStackNavigator();
 const OrderNavgigator = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="OrdersList" component={Orders} />
-      <Stack.Screen name="MoreOrderItemDetails" component={OrderCard} />
+      <Stack.Screen name="OrderDetails" component={MainOrderDetails} />
     </Stack.Navigator>
   );
 };
